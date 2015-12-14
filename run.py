42c58001a23f92026ad15ab44e74cd8310c12083
Choonho Son <choonho.son@gmail.com>
2015-12-14 11:40:07 +0000

    Add programmable progress
    Signed-off-by: Choonho Son <choonho.son@gmail.com>

--- run.py
+++ run.py
@@ -13,6 +13,14 @@ def hello_world():
 def install():
 	return render_template('install.html', step=5)
 
+@app.route('/install2')
+def install2():
+    step = {'1': {'step':'step-1', 'Title':'Deploy Git', 'percent':30, 'desc':'aaaaaaa'},
+            '2': {'step':'step-2', 'Title':'Deploy Gerrit', 'percent':50, 'desc':'bbbbbbb' },
+            '3': {'step':'step-3', 'Title':'Deploy Jenkins', 'percent':100, 'desc':'ccccccc' }} 
+    return render_template('install2.html', progress=step)
+
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
